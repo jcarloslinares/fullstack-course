e7a285e541d695945792a9ff514933b920339d72
Iamat <jcarlos@iamat.com>
2017-04-28 10:05:21 -0300

    ejercicio 5.1

---
+++ clase1/ejercicios/ej5.1.js
@@ -0,0 +1,48 @@
+/*
+* En un archivo ej5.json, guardar todas los nombres de los planetas que aparecen en la pelicula "Attack of the Clones"
+* 
+* 1) Obtener los ids de los planetas leyendo el json del archivo planets.json buscando por id=5
+* 2) Obttener los planetas del archivo planets.json
+* 3) Escribir el resultado en un nuevo archivo (ej5.json) con el siguiente formato:
+* 
+* ["planeta1", "planeta2", ...]
+*
+* Home work: relizar este ejercicio usando las distintas tecnicas vistas en clase
+* Guardar las distintas versiones en archivos ej5.1.js, ej5.2.js, ej5.3.js, ej5.4.js
+*/
+
+const fs = require('fs')
+
+// constantes con las ubicaciones de los archivos
+const planetsFile = '../data/planets.json'; 
+const filmsFile = '../data/films.json'; //buscar id=5
+const ej1File = './ej1.json';
+
+//read the films file to found id planets 
+fs.readFile(filmsFile,'utf-8', (err,data)=>{
+    if(err){
+        console.log(`Error leyendo el archivo ${filmsFile}`);
+    }
+    const films = JSON.parse(data);
+
+    const movie =  films.find(elem => elem.id === '5')
+    // console.log(1, movie)
+
+    fs.readFile(planetsFile, 'utf-8', (err, data)=>{
+        if(err){
+            console.log(`Error leyendo el archivo ${planetsFile}`)
+        }
+
+        //console.log(data)
+        var planets = JSON.parse(data);
+        // console.log(2, planets)
+                                   // .filter(film => (luke.films.indexOf(film.id) >= 0))
+        const planetsMovie = planets.filter(planet => (movie.planets.indexOf(planet.id) >= 0 ))
+        .map(planet => planet.name )
+
+        console.log(planetsMovie)
+
+    })
+
+
+})
